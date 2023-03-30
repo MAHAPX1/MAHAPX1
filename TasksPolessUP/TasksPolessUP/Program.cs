09037d8105529fc2c0a29f6cded4429c32d4eb66
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-03-30 17:12:41 +0300

    Первое задание по С# Напечатать лицо

---
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -0,0 +1,9 @@
+﻿char ch = '"';
+string str =
+    " +" + ch + ch + ch + ch + ch + "+\n" +
+    "(| o o |)\n" +
+    " |  ^  |\n" +
+    " | '-' |\n" +
+    " +-----+"; 
+Console.WriteLine(str);
+Console.ReadKey();
