6e9154f636eb791532861404333871adf20cbaad
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-04-01 09:47:19 +0300

    Пятое задание С#

--- TasksPolessUP/TasksPolessUP/Program.cs
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -1,31 +1,25 @@
-﻿// Четвертое задание C#
+﻿// Пятое задание C#
 
-// Ввод: nums = [2,3,1]
-// Вывод: 1
-// Объяснение:
-// Первый отчетливый максимум равен 3.
-// Второй отчетливый максимум равен 2.
-// Третий отчетливый максимум равен 1.
+/* Реализуйте алгоритм, который находит элемент с максимальным значением. Не используйте LINQ. 
+Ввод: nums = [2, 3, 1]
+Вывод: 1
+Объяснение: Поскольку 3 является максимальным числом, то выводим его индекс.
+*/
+int[] nums = { 10, 8, 70, int.MaxValue, 9, 11};
 
-int[] nums = { 100, int.MaxValue, 80, 25, 8, int.MinValue, 30, 41, 40, 20};
-
-int count = 3; // количество максимумов. В условии 3, но если поставить 6, то выведет шестой максимум
-
-    for (int j = 1; j <= count; j++)
-    {
-        int max = int.MinValue;
-        for (int i = 0; i <= nums.Length - j ; i++)
+int max = int.MinValue;
+int index = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+        if (nums[i] > max)
         {
-            if (nums[i] > max)
-            {
-                max = nums[i];
-                nums[i] = nums[nums.Length - j ];
-                nums[nums.Length - j ] = max;
-                --i;
-            }
+           max = nums[i];
+           index = i;                          
         }
-        Console.WriteLine(max);
-    }
+}
+
+Console.WriteLine(index);
+
 
 Console.ReadKey();
 
