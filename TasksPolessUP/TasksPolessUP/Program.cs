627d65bf0d6471663fc65ed0256b4323b56ba0aa
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-04-01 09:29:11 +0300

    Четвертое задание C#

--- TasksPolessUP/TasksPolessUP/Program.cs
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -1,21 +1,31 @@
-﻿
-// Третье задание C#
+﻿// Четвертое задание C#
 
-// Ввод: числа = [1,3,5,6], цель = 5
-// Вывод: 2
+// Ввод: nums = [2,3,1]
+// Вывод: 1
+// Объяснение:
+// Первый отчетливый максимум равен 3.
+// Второй отчетливый максимум равен 2.
+// Третий отчетливый максимум равен 1.
 
-int[] nums = { 1, 3, 4, 7, 10, 20 };
-int target = 10;
+int[] nums = { 100, int.MaxValue, 80, 25, 8, int.MinValue, 30, 41, 40, 20};
 
-if (target >= nums[^1]) Console.WriteLine(nums.Length);
-else 
-    for (int i = 0; i < nums.Length; i++)
+int count = 3; // количество максимумов. В условии 3, но если поставить 6, то выведет шестой максимум
+
+    for (int j = 1; j <= count; j++)
     {
-        if (nums[i] >= target && target <= nums[i] )
+        int max = int.MinValue;
+        for (int i = 0; i <= nums.Length - j ; i++)
         {
-            Console.WriteLine(i);
-            break;
+            if (nums[i] > max)
+            {
+                max = nums[i];
+                nums[i] = nums[nums.Length - j ];
+                nums[nums.Length - j ] = max;
+                --i;
+            }
         }
+        Console.WriteLine(max);
     }
 
 Console.ReadKey();
+
