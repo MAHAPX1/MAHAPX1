75eec90a072fb6ef005a7f71faa9f091cdd1e6b5
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-04-03 17:42:41 +0300

    Девятое задание C#

--- TasksPolessUP/TasksPolessUP/Program.cs
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -1,8 +1,10 @@
-﻿// Восьмое задание C#
+﻿// Девять задание C#
 
 /* Реализуйте метод
-Ввод:(255,255,255)
-Вывод: "FFFFFF"
+Ввод: 2000
+Вывод: true
+Ввод: 2015
+Вывод: false
 */
 
 namespace TasksPolessUP
@@ -11,45 +13,22 @@ namespace TasksPolessUP
     {
         static void Main(string[] args)
         {
-            string rgb = "10,78,2595"; // Несколько тестовых вариантов ввода строки
-           // string rgb = "10,678,-9";
-           // string rgb = "10 678,90";
-            // string rgb = "10,90 ";
-            Console.WriteLine(RGB(rgb));
+            int year = 2015;
+            Console.WriteLine(Visokos(year));
+
             Console.ReadKey();
         }
 
-        static string RGB(string rgb)
+        static bool Visokos(int year)
         {
-            rgb += ",";
-            string res = string.Empty;
-            string strnumber = string.Empty;
-            int number;
-
-            for (int i = 0; i < rgb.Length; i++)
+            if (year % 4 == 0)
             {
-                if (char.IsDigit(rgb[i]))
-                {
-                    strnumber += rgb[i];
-                }
-                else if (rgb[i] == ',')
-                {            
-                    try
-                    {
-                        number = Convert.ToInt32(strnumber);
-                        res += number > 255 ? "ff," : Convert.ToString(number, 16) + ',';
-                    }
-                    catch
-                    {
-                        return "Исходная строка ведена неверно";
-                    }
-                    strnumber = string.Empty;                   
-                }
-                else return "Исходная строка ведена неверно";
-
+                return true;
             }
-
-            return res[..^1];
+            else
+            {
+                return false;
+            }
         }
     }
 }
