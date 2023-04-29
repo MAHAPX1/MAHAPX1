89c2d6ba870f76959808d2e9dc17fb7c26d7a35e
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-04-29 12:41:04 +0300

    18 Задание C#

--- TasksPolessUP/TasksPolessUP/Program.cs
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -1,41 +1,67 @@
 ﻿
-// 17 задание C#
+// 18 задание C#
 /* 
-    Ввод: s = "a1b2"
-    Вывод: ["a1b2", "a1B2", "A1b2", "A1B2"]
+    Действительный IPv4- адрес — это IP-адрес в форме где и не может содержать начальные нули.
+    Например, и являются допустимыми адресами IPv4 , а , и являются недопустимыми адресами
+    IPv4."x1.x2.x3.x4"0 <= xi <= 255xi "192.168.1.1""192.168.1.0""192.168.01.1""192.168.1.00""192.168@1.1
 */
 
 namespace TasksPolessUP
 {
     class Program
     {
+        static string CheckIp(string ip)
+        {           
+            string[] number = ip.Split('.', ':');
+            List<int> Ip = new List<int> ();
+            foreach (var item in number)
+            {
+                try
+                {
+                    Ip.Add(Convert.ToInt32(item, 16));
+                }
+                catch (Exception)
+                {
+                    return "Neither";
+                }
+                
+            }
+            int IPv4 = 4, IPv6 = 8;
+            if (Ip.Count == IPv4)
+            {
+                for (int i = 0; i < Ip.Count; i++)
+                {
+                    if (Ip[i] < 0 || 597 < Ip[i])
+                    {
+                        return "Neither";
+                    }
+                }
+            }
+            else if (Ip.Count == IPv6)
+            {
+                for (int i = 0; i < Ip.Count; i++)
+                {
+                    if (Ip[i]> 0xffff || 0x0 > Ip[i])
+                    {
+                        return "Neither";
+                    }
+                }
+            }
+            return "Yes";
+
+        }
         static void Main(string[] args)
         {
-            string s = "a1b23d";
-            //string s = "abCd";
-            //string s = "ABC";
+            string ip = "192.168@1.1";      //False
+            //string ip = "192.168.01.1";   //True
+           
+            //string ip = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";    //True
+            //string ip = "2001:0db8:85a3::0:8a2e:0370:7334";           //False
+            //string ip = "2001:0db8:85a3:01000000:0:8a2e:0370:7334";   //False
+           
+            Console.WriteLine(CheckIp(ip)); 
           
-            string news;
-            s = s.ToLower();
-            List<string> list = new List<string>() { s };
-            
-            for (int i = 0; i < s.Length;)
-            {
-                news = s;
-                for (int j = i + 1; j < s.Length;j++)
-                {
-                    list.Add(news[..j]);
-                    list[^1] += char.ToUpper(news[j]) + news[++j..];
-
-                    --j;
-                }
-                s = s[..i] + char.ToUpper(s[i]) + s[++i..];
-                list.Add(s);
-            }
-            foreach (var item in list.Distinct())
-            {
-                Console.WriteLine(item);
-            }
+           
             Console.ReadKey();
         }
 
