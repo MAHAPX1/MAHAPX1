c9e8fa8fc8233c8acd82598722bcde97c626cb05
MAHAPX1 <dimon2015pinsk@gmail.com>
2023-05-03 16:17:31 +0300

    21 Задание C#

--- TasksPolessUP/TasksPolessUP/Program.cs
+++ TasksPolessUP/TasksPolessUP/Program.cs
@@ -1,36 +1,33 @@
 ﻿
-// 20 задание C#
+// 21 задание C#
 /* 
-    Учитывая целое число n, вернуть количество простых чисел, которые строго меньше, чем n.
-    Вход: n = 10
-    Выход: 4
+    Учитывая координаты двух прямолинейных прямоугольников в 2D-плоскости, верните общую площадь, покрытую двумя прямоугольниками.
+    Первый прямоугольник определяется своим нижним левым и (ax1, ay1) верхним правым углами (ax2, ay2).
+    Второй прямоугольник определяется левым нижним и (bx1, by1) правым верхним углами (bx2, by2).
+
+    Ввод: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2 
+    Вывод: 45 ((((ЭТО НЕ ПРАВИЛЬНО))))
 */
 
 namespace TasksPolessUP
 {
     class Program
-    {
-        static int SimpleCount(int n)
-        {
-            int count = n >= 2 ? 1: 0;
-            bool flag;
-            for (int i = n; i > 2; i--)
-            {
-                flag = false;
-                for (int j = 2; j < i; j++)
-                {
-                 if (i % j == 0) break;
-                 if (j + 1 == i) flag = true;
-                } 
-                if (flag) count++;
-            }
-            return count;
-        }
+    {    
         static void Main(string[] args)
         {
-           int n = 10;
-           Console.WriteLine("Количество простых чисел " + SimpleCount(n));
-           Console.ReadKey();
+            // Исходные данные из условия
+            Rectangle r1 = new Rectangle(-3, 0, 3, 4);
+            Rectangle r2 = new Rectangle(0, -1, 9, 2);
+
+            // Пример где прямоугольники не соприкасаются
+            //Rectangle r1 = new Rectangle(1, 1, 3, 3);
+            //Rectangle r2 = new Rectangle(3, 3, 4, 5);
+
+            // Просто пример для теста
+            //Rectangle r1 = new Rectangle(1, 1, 3, 6);
+            //Rectangle r2 = new Rectangle(2, 3, 5, 5);
+            Console.WriteLine(Rectangle.SquareTwo(r1, r2));
+            Console.ReadKey();
         }
 
     }
---
+++ TasksPolessUP/TasksPolessUP/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksPolessUP
+{
+    class Rectangle
+    {
+        int x1; 
+        int y1;
+        int x2; 
+        int y2;
+
+        static public int SquareTwo(Rectangle r1, Rectangle r2)
+        {
+            int x = 0, y = 0;
+            for (int i = r1.x1; i < r1.x2; i++)
+            {
+                  if(i >= r2.x1 && i < r2.x2) x++;                
+            }
+            for (int i = r1.y1; i < r1.y2; i++)
+            {
+                if (i >= r2.y1 && i < r2.y2) y++;               
+            }
+            return x * y;
+        }
+        public Rectangle(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+}
